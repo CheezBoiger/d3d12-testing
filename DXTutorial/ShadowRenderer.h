f26db9eefb44ec2536f6bd04857cde1c24009739
mariogar <mario.garcia@intel.com>
2020-02-10 17:10:34 -0800

    add shadow design.

--- DXTutorial/ShadowRenderer.h
+++ DXTutorial/ShadowRenderer.h
@@ -1,1 +1,56 @@
 #pragma once
+
+
+#include "WinConfigs.h"
+#include "BackendRenderer.h"
+#include "Math/Bounds3D.h"
+#include "GraphicsResources.h"
+#include "FrontEndRenderer.h"
+#include "GlobalDef.h"
+
+namespace jcl {
+namespace Shadows {
+
+
+// Light Shadow is the object that holds the info to the shadow atlas, along with other 
+// info regarding the shadow to be drawn.
+struct LightShadow
+{
+    // Bounds of the Shadow. To be used for culling.
+    Bounds3D _worldBounds;
+
+    struct {
+        // The perspective of the light.
+        Matrix44 _viewToClipSpace;
+        // The id of the shadow in the atlas.
+        U32 id;
+    } _toGpu;
+};
+
+void initializeShadowRenderer(gfx::BackendRenderer* pRenderer);
+void cleanUpShadowRenderer(gfx::BackendRenderer* pRenderer);
+
+void generateShadowCommands
+    (
+        // Front End renderer.
+        FrontEndRenderer* pRenderer,
+        // list to record our commands to. 
+        gfx::CommandList* pList,
+        // Meshes to render onto this gpass.
+        GeometryMesh** pMeshes,
+        // Number of meshes in this mesh array.
+        U32 meshCount,
+        //
+        GeometrySubMesh** pSubMeshes,
+        //
+        U32 submeshCount
+    );
+// Generate the shadow resolve. This is to be used for sunlight only!
+// 
+void generateShadowResolveCommand(gfx::CommandList* pList);
+
+// Retrieve the shadow atlas used by the lighting stage.
+gfx::Resource* getShadowAtlas();
+gfx::RenderTargetView* getShadowResolve();
+} // Shadows
+} // jcl
